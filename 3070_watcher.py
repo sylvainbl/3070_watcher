580487c2cb9da6047c51a96d447a92efd28dea56
sylvainbl <sylvain.abbg@gmail.com>
2020-11-21 14:18:45 +0100

    function return true/false if price low or not

--- 3070_watcher.py
+++ 3070_watcher.py
@@ -2,15 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL = 'https://www.gputracker.eu/fr/search/category/1/cartes-graphiques?onlyInStock=true&fv_gpu.chip=NVIDIA%20RTX%203070'
-page = requests.get(URL)
+def find_prices():
+    URL = 'https://www.gputracker.eu/fr/search/category/1/cartes-graphiques?onlyInStock=true&fv_gpu.chip=NVIDIA%20RTX%203070'
+    page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    array_prices = []
 
-prices = soup.find_all(
-    class_='font-weight-bold text-secondary w-100 d-block h1 mb-2')
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    prices = soup.find_all(
+        class_='font-weight-bold text-secondary w-100 d-block h1 mb-2')
+
+    for price in prices:
+        price_elem = price.find('span')
+        array_prices.append(int(price_elem.text))
+
+    print(array_prices)
+    return array_prices
 
 
-for price in prices:
-    price_elem = price.find('span')
-    print(price_elem.text, end='\n'*2)
+def analyse_prices(array_prices):
+    if min(array_prices) < 580:
+        return True
+    return False
+
+
+print(analyse_prices(find_prices()))
